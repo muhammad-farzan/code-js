5449ac848b5fd85d727fd6dcbcad2e3dec13e2ee
Muhammad Farzan <126473267+muhammad-farzan@users.noreply.github.com>
2024-12-19 15:53:41 +0000

    variable file done

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "muhammadFarzan@gmail.com"
+var accountPassword = "987654321"
+accountcity = "karachi"
+
+accountId = 2
+
+console.log(accountId);
---
+++ variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "muhammadFarzan@gmail.com"
+var accountPassword = "987654321"
+accountcity = "karachi"
+
+console.log("accountId");
+
